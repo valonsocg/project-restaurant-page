296431ecabd441c5e212e994f7edc812d55b08ad
valonsocg <valonsocg@gmail.com>
2024-08-10 12:38:41 -0500

    Initial structure, webpack config, plugins and loaders, create a initial page component

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import component from "./initial-page-load";
+
+component();
---
+++ src/initial-page-load.js
@@ -0,0 +1,22 @@
+import Img from "./assets/la-plaza-bar-grill.jpg";
+
+export default function component() {
+  const content = document.querySelector(".content");
+  const title = document.createElement("h1");
+  const img = new Image();
+  img.src = Img;
+  const subtitle = document.createElement("h2");
+  const description = document.createElement("p");
+
+  title.textContent = "Traditional Food 'La Benita' Restaurant";
+  subtitle.textContent = "Welcome to 'La Benita'";
+  description.textContent =
+    "At La Benita, we believe in serving up only the finest, most mouth-watering dishes. Our chefs use only the freshest ingredients to create meals that are both beautiful and delicious. Whether you're in the mood for a quick bite or a full-course meal, we've got something for everyone. Come visit us today and experience the best food in town!";
+
+  content.appendChild(title);
+  content.appendChild(img);
+  content.appendChild(subtitle);
+  content.appendChild(description);
+
+  return content;
+}
